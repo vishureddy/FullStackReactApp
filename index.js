64c838e5bad1f464fb321e93f9b8bc7a4d27de50
vishureddy <viswaswara@gmail.com>
2021-01-04 14:35:18 +0530

    Changes has done to index.js files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("");
+  res.send("Its my first deployment in Heroku");
 });
 
 const PORT = process.env.PORT || 5000;
